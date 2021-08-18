5fbc4f4fddae49d2c59fccc620667d989c5cbd1d
ailisc97 <ailiscurran97@gmail.com>
2021-08-18 10:52:33 +0000

    fixed flask

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from flask import flask
+from flask import Flask
 
 app=Flask(__name__)
 
@@ -14,3 +14,4 @@ if __name__ == "__main__":
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
+        
